7d2726dba6f3abfaa1507eb76b30db47f50e6886
Subinkrishna Gopi <subinkrishna.gopi@gmail.com>
2016-02-05 14:32:25 -0700

    Added AvatarView.java to quick_samples

---
+++ quick_samples/AvatarView.java
@@ -0,0 +1,51 @@
+package com.subinkrishna.customviews.widget;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+
+import com.subinkrishna.widget.CircularImageView;
+
+/**
+ * @author Subinkrishna Gopi
+ */
+public class AvatarView extends CircularImageView {
+
+    public AvatarView(Context context) {
+        this(context, null, 0);
+    }
+
+    public AvatarView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public AvatarView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        // Disabling check state animations all together
+        allowCheckStateAnimation(false);
+    }
+
+    @Override
+    protected boolean shouldDrawBorder() {
+        // Draw border only when no drawable is set
+        return null != getDrawable();
+    }
+
+    @Override
+    protected Paint getCheckMarkPaint() {
+        Paint paint = super.getCheckMarkPaint();
+        // Overriding stroke color
+        paint.setColor(Color.YELLOW);
+        return paint;
+    }
+
+    @Override
+    protected int getCheckMarkStrokeWidthInPixels() {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                5, // DIPs
+                getResources().getDisplayMetrics());
+    }
+
+}
